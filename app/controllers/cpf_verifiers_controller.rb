b100adbe07e5f830c158886f9b08826c6816d641
João Souza <joaomarcelo9393@gmail.com>
2024-06-26 16:39:54 -0300

    verificar cpf

---
+++ app/controllers/cpf_verifiers_controller.rb
@@ -0,0 +1,17 @@
+class CpfVerifiersController < ApplicationController
+  def new
+  end
+
+  def verify
+    cpf = params["cpf"]
+    file = File.open("lib/assets/cpfs_validos.txt")
+    file_data = file.readlines.map(&:chomp)
+    cpf_valid = file_data.include?(cpf)
+
+    if cpf_valid
+      redirect_to "/users/sign_up"
+    else
+      redirect_to "/cpf/verificar", alert: "CPF inválido"
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
+
+  get "cpf/verificar", to: "cpf_verifiers#new"
+  post "cpf/verificar", to: "cpf_verifiers#verify"
 end
